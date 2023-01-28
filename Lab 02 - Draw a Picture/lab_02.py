2db25a7931d5b14c06e2e6221b6dfb2150a82a4a
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-01-28 16:42:35 -0600

    put in comments of what each thing does

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -2,15 +2,21 @@ import arcade
 arcade.open_window(600, 600, "Drawing Example")
 arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 arcade.start_render()
+#draws the grass
 arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+#draws the orange tree
 arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.ORANGE)
 arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.ORANGE)
+#draws the pink tree
 arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.PINK)
 arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.PINK)
+#draws the blue tree
 arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.BLUE)
 arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.BLUE, 0, 180)
+#draws the yellow tree
 arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.YELLOW)
 arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.YELLOW)
+#draws the purple tree
 arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.PURPLE)
 arcade.draw_polygon_filled(((500, 400),
                             (480, 360),
@@ -19,6 +25,7 @@ arcade.draw_polygon_filled(((500, 400),
                             (520, 360)
                             ),
                            arcade.csscolor.PURPLE)
+#draws the sun
 arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
 arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)
 arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
