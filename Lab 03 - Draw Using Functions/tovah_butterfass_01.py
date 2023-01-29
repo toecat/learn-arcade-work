b234c1cefb1c18427ca7c815108bbc624cb40dd4
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-01-29 14:34:01 -0600

    adds frame around dog

--- Lab 03 - Draw Using Functions/tovah_butterfass_01.py
+++ Lab 03 - Draw Using Functions/tovah_butterfass_01.py
@@ -1,15 +1,17 @@
 import arcade
 arcade.open_window(800, 800, "Dog")
-arcade.set_background_color(arcade.csscolor.WHITE)
+arcade.set_background_color(arcade.csscolor.ROSY_BROWN)
 arcade.start_render()
+arcade.draw_circle_filled(400, 400, 350, arcade.color.DUTCH_WHITE)
+arcade.draw_circle_outline(400, 400, 350, arcade.color.BLACK)
 # draws head
 arcade.draw_circle_filled(400, 400, 200, arcade.color.BEAVER)
-arcade.draw_circle_outline(400, 400, 200, arcade.color.BLACK_BEAN)
+arcade.draw_circle_outline(400, 400, 200, arcade.color.BLACK)
 # draws ears
 arcade.draw_ellipse_filled(235, 325, 100, 420, arcade.color.BEAVER)
 arcade.draw_ellipse_filled(565, 325, 100, 420, arcade.color.BEAVER)
-arcade.draw_ellipse_outline(235, 325, 100, 420, arcade.color.BLACK_BEAN)
-arcade.draw_ellipse_outline(565, 325, 100, 420, arcade.color.BLACK_BEAN)
+arcade.draw_ellipse_outline(235, 325, 100, 420, arcade.color.BLACK)
+arcade.draw_ellipse_outline(565, 325, 100, 420, arcade.color.BLACK)
 # draws eyes
 arcade.draw_circle_filled(350, 450, 40, arcade.color.WHITE)
 arcade.draw_circle_filled(450, 450, 40, arcade.color.WHITE)
