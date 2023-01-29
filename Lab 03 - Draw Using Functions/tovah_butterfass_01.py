70996562f39544998d5cfd99964e62286eda12ac
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-01-29 14:36:42 -0600

    ^

--- Lab 03 - Draw Using Functions/tovah_butterfass_01.py
+++ Lab 03 - Draw Using Functions/tovah_butterfass_01.py
@@ -2,7 +2,7 @@ import arcade
 arcade.open_window(800, 800, "Dog")
 arcade.set_background_color(arcade.csscolor.ROSY_BROWN)
 arcade.start_render()
-arcade.draw_circle_filled(400, 400, 350, arcade.color.DUTCH_WHITE)
+arcade.draw_circle_filled(400, 400, 350, arcade.color.WHITE)
 arcade.draw_circle_outline(400, 400, 350, arcade.color.BLACK)
 # draws head
 arcade.draw_circle_filled(400, 400, 200, arcade.color.BEAVER)
