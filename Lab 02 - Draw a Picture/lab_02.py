8be92f2f9a9cc10feb225d3c39113593a0572aed
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-01-30 09:23:43 -0600

    changes colors of trunks

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -5,19 +5,19 @@ arcade.start_render()
 #draws the grass
 arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
 #draws the orange tree
-arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.ORANGE)
+arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
 arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.ORANGE)
 #draws the pink tree
-arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.PINK)
+arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
 arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.PINK)
 #draws the blue tree
-arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.BLUE)
+arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.SIENNA)
 arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.BLUE, 0, 180)
 #draws the yellow tree
-arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.YELLOW)
+arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.SIENNA)
 arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.YELLOW)
 #draws the purple tree
-arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.PURPLE)
+arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.SIENNA)
 arcade.draw_polygon_filled(((500, 400),
                             (480, 360),
                             (470, 320),
