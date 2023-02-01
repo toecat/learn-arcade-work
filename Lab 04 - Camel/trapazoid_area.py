0dec13432f2ce65269a3b353762482e66b03cf83
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-01 09:19:20 -0600

    finished

---
+++ Lab 04 - Camel/trapazoid_area.py
@@ -0,0 +1,6 @@
+print("Area of a trapazoid:")
+height = int(input("What is the height of the trapazoid? "))
+bottom_base = int(input("What is the length of the bottom base? "))
+top_base = int(input("What is the length of the top base? "))
+area = ((top_base + bottom_base)/2) * height
+print("Area:" ,area)
