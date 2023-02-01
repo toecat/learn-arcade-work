a4d94c77909271aec10b4fdc05fc0f79edeba0e4
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-01 09:33:29 -0600

    finished

---
+++ Lab 04 - Camel/calculator.py
@@ -0,0 +1,4 @@
+miles_drive = int(input("Number of miles you are going to drive: "))
+speed = int(input("What is the averge speed you are going to be going in mph? "))
+amount_of_time = miles_drive / speed 
+print("The time it is going to take you to get there is", amount_of_time, "hours")
