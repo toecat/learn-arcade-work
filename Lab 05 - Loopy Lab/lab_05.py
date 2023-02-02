02b5dc696b6ac5f827e763ec543499af8f4593fb
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-02 09:34:13 -0600

    volume of prism

---
+++ Lab 05 - Loopy Lab/lab_05.py
@@ -0,0 +1,7 @@
+def volume_prism():
+    length = int(input("What is the legnth of the prism? "))
+    width = int(input("What is the width of the prism? "))
+    height = int(input("What is the height of the prism?"))
+    print("The volume is" ,(length * width) * height)
+    
+volume_prism()
