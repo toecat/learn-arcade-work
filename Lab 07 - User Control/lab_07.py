6e4d49c57e64002680aec2d96a9511d19828ad92
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-07 10:12:08 -0600

    3 sets of trees

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -1,52 +1,43 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+arcade.open_window(600, 600, "Drawing Example")
 
 
-def draw_grass():
-    """ Draw the ground """
-    arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.color.AIR_SUPERIORITY_BLUE)
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 
-
-def draw_snow_person(x, y):
-    """ Draw a snow person """
-
-    # Draw a point at x, y for reference
-    arcade.draw_point(x, y, arcade.color.RED, 5)
-
-    # Snow
-    arcade.draw_circle_filled(x, 60 + y, 60, arcade.color.WHITE)
-    arcade.draw_circle_filled(x, 140 + y, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(x, 200 + y, 40, arcade.color.WHITE)
-
-    # Eyes
-    arcade.draw_circle_filled(x - 15, 210 + y, 5, arcade.color.BLACK)
-    arcade.draw_circle_filled(x + 15, 210 + y, 5, arcade.color.BLACK)
-
-
-def on_draw(delta_time):
-    """ Draw everything """
-    arcade.start_render()
-
-    draw_grass()
-    draw_snow_person(150, 140)
-    draw_snow_person(on_draw.snow_person1_x, 140)
-    draw_snow_person(450, 180)
-
-    on_draw.snow_person1_x += 1
-
-
-on_draw.snow_person1_x = 150
-
+arcade.start_render()
 def main():
-    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-    arcade.set_background_color(arcade.color.DARK_BLUE)
+    arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
 
-    # Call on_draw every 60th of a second.
-    arcade.schedule(on_draw, 1/60)
-    arcade.run()
+    def draw_trees(x, y):
+        arcade.draw_rectangle_filled(100 + x, 320 + y, 20, 60, arcade.csscolor.SIENNA)
+        arcade.draw_circle_filled(100 + x, 350 + y, 30, arcade.csscolor.DARK_GREEN)
+        arcade.draw_rectangle_filled(200 + x, 320 + y, 20, 60, arcade.csscolor.SIENNA)
+        arcade.draw_ellipse_filled(200+ x, 370 + y, 60, 80, arcade.csscolor.DARK_GREEN)
+        arcade.draw_rectangle_filled(300 + x, 320 + y, 20, 60, arcade.csscolor.SIENNA)
+        arcade.draw_arc_filled(300 + x, 340 + y, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
+        arcade.draw_rectangle_filled(400 + x, 320 + y, 20, 60, arcade.csscolor.SIENNA)
+        arcade.draw_triangle_filled(400 + x, 400 + y, 370 + x, 320 + y, 430 +x, 320 + y, arcade.csscolor.DARK_GREEN)
+        arcade.draw_rectangle_filled(500 + x, 320 + y, 20, 60, arcade.csscolor.SIENNA)
+        arcade.draw_circle_filled(500 + x, 360 + y, 30, arcade.csscolor.DARK_GREEN)
 
 
-# Call the main function to get the program started.
+    def draws_sun(a, b):
+        arcade.draw_circle_filled(500 + a, 550 + b, 40, arcade.color.YELLOW)
+        arcade.draw_line(500 + a, 550 + b, 400 + a, 550 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 600 + a, 550 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 500 + a, 450 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 500 + a, 650 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 550 + a, 600 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 550 + a, 500 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 450 + a, 600 + b, arcade.color.YELLOW, 3)
+        arcade.draw_line(500 + a, 550 + b, 450 + a, 500 + b, arcade.color.YELLOW, 3)
+
+    draws_sun(-40, -60)
+    draw_trees(0, -130)
+    draw_trees(0, -250)
+    draw_trees(0,0)
 main()
+arcade.finish_render()
+
+arcade.run()
