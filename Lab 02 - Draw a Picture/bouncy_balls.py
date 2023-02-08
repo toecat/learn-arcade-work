000c45c04ee913d88d60fd56bf2306db1127b5bd
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-08 11:11:41 -0600

    summative

---
+++ Lab 02 - Draw a Picture/bouncy_balls.py
@@ -0,0 +1,3 @@
+import arcade
+arcade.open_window(1000, 1000, "Rabbit")
+arcade.run()
--- Lab 02 - Draw a Picture/summative.py
+++ Lab 02 - Draw a Picture/summative.py
@@ -1,15 +1,19 @@
 import arcade
+
 arcade.open_window(900, 900,"Frog")
 arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 arcade.start_render()
+
+
 def draws_frog():
     arcade.draw_ellipse_filled(300, 500, 300, 200, arcade.csscolor.GREEN)
     arcade.draw_ellipse_filled(170, 420, 80, 130, arcade.csscolor.GREEN, 30)
     #arcade.draw_ellipse_outline(200, 440, 50, 100, arcade.csscolor.BLACK, 2, 30)
     arcade.draw_ellipse_filled(190, 390, 130, 80, arcade.csscolor.GREEN)
-    
 
-
+def draw_flowers():
+    arcade.draw_circle_filled(100, 400, 40, arcade.csscolor.SEA_GREEN)
 draws_frog()
+draw_flowers()
 arcade.finish_render()
 arcade.run()
