45335bccade8a644764ad5afe1aa7871600d5d2c
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-08 10:59:26 -0600

    frog

--- Lab 02 - Draw a Picture/summative.py
+++ Lab 02 - Draw a Picture/summative.py
@@ -4,10 +4,10 @@ arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 arcade.start_render()
 def draws_frog():
     arcade.draw_ellipse_filled(300, 500, 300, 200, arcade.csscolor.GREEN)
-    arcade.draw_ellipse_filled(200, 440, 50, 100, arcade.csscolor.GREEN, 30)
-    arcade.draw_ellipse_outline(200, 440, 50, 100, arcade.csscolor.BLACK, 2, 30)
-    arcade.draw_ellipse_filled(220, 410, 100, 50, arcade.csscolor.GREEN)
-    arcade.draw_ellipse_outline(220, 410, 100, 50, arcade.csscolor.BLACK, 2)
+    arcade.draw_ellipse_filled(170, 420, 80, 130, arcade.csscolor.GREEN, 30)
+    #arcade.draw_ellipse_outline(200, 440, 50, 100, arcade.csscolor.BLACK, 2, 30)
+    arcade.draw_ellipse_filled(190, 390, 130, 80, arcade.csscolor.GREEN)
+    
 
 
 draws_frog()
