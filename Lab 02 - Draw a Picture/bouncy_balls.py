60edf9d43a63fc0a647185f25b285004245e06f7
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-10 09:53:51 -0600

    finished

--- Lab 02 - Draw a Picture/bouncy_balls.py
+++ Lab 02 - Draw a Picture/bouncy_balls.py
@@ -1,7 +1,9 @@
 import arcade
 SCREEN_WIDTH=800
 SCREEN_HEIGHT=800
-
+def tree(a,b):
+    arcade.draw_rectangle_filled(a, b, 60, 260, arcade.csscolor.SIENNA)
+    arcade.draw_circle_filled(a, b+90, 90, arcade.csscolor.SEA_GREEN)
 def draw_dog(x,y):
     arcade.draw_ellipse_filled(400+x, 400+y, 200, 100, arcade.color.BEAVER)
     arcade.draw_line(350+x, 400+y, 350+x, 315+y, arcade.color.BEAVER, 5)
@@ -14,15 +16,23 @@ def draw_dog(x,y):
     arcade.draw_ellipse_filled(480+x, 430+y, 30, 70, arcade.color.BEAVER)
     arcade.draw_ellipse_outline(480+x, 430+y, 30, 70, arcade.color.BLACK)
     arcade.draw_lrtb_rectangle_filled(0, 799, 320, 0, arcade.csscolor.GREEN)
+def draw_sun():
+    arcade.draw_circle_filled(800, 800, 100, arcade.csscolor.YELLOW)
 
 def draw_ball(x, y, z):
     arcade.draw_circle_filled(x, y, 10, z)
 def on_draw(delta_time):
     arcade.start_render()
+    draw_sun()
+    tree(100, 400)
+    tree(300, 400)
+    tree(500, 400)
+    tree(700, 400)
+    
     draw_ball(on_draw.ball_x, 450, arcade.csscolor.YELLOW)
     draw_dog(on_draw.dog_x, 0)
     #draw_ball(on_draw.ball_x, 450, arcade.csscolor.PINK)
-    
+
     
     on_draw.ball_x += 2
     on_draw.dog_x += 1
@@ -35,6 +45,7 @@ def main():
     arcade.open_window(SCREEN_WIDTH,SCREEN_HEIGHT, "Window")
     arcade.set_background_color((48, 78, 240))
     arcade.schedule(on_draw, 1/60)
+    
     
     arcade.run()
 
