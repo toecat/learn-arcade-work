276530f6e39e8eeaac74e28ad0425427967bf46a
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-14 09:54:25 -0600

    ..

--- Lab 03 - Draw Using Functions/quiz.py
+++ Lab 03 - Draw Using Functions/quiz.py
@@ -1,6 +1,7 @@
 correct = 0
 print("Take my quiz")
 print()
+
 answer = str(input('What is my name? '))
 if answer.lower() == "tovah":
     print("Correct")
@@ -10,7 +11,8 @@ else:
     print("No")
     print(correct)
 print()
-answer = int(input("How old am I? "))
+
+answer = int(input("How old is Tovah? "))
 if answer == 14:
     print("Correct")
     correct +=1
@@ -19,6 +21,7 @@ else:
     print("No")
     print(correct)
 print()
+
 answer = str(input("Which football team is my favorite? "))
 if answer.lower() == "packers":
     print("Correct")
@@ -28,6 +31,7 @@ else:
     print("No")
     print(correct)
 print()
+
 answer = str(input("Who won the superbowl? "))
 if answer.lower() == "chiefs":
     print("Correct")
@@ -37,6 +41,7 @@ else:
     print("No")
     print(correct)
 print()
+
 answer = int(input("How many points did the Chiefs win by? "))
 if answer == 3:
     print("Correct")
@@ -46,5 +51,6 @@ else:
     print("No")
     print(correct)
 print()
+
 total = (correct/5)*100
 print("You got", total, "% of the questions correct, good job!")
