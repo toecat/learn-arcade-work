867d436337de3252b16e61e6b405a775efb15394
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-20 10:08:13 -0600

    save

---
+++ Lab 04 - Camel/lab_04.py
@@ -0,0 +1,19 @@
+import random
+print("Welcome to the world!")
+print("It is time to start your life and get away from your family.")
+print("Your parents are trying to catch you.")
+print("Run away from them without getting hurt:")
+
+
+print("hi")
+a = "Get a drink and eat some food"
+b = "Crawl ahead"
+c = "Run ahead"
+d = "Stop and take a nap"
+e = "Status check"
+q = quit
+x = str(input("Choose an option: "))
+if x=="a":
+    print("drink and food")
+
+
