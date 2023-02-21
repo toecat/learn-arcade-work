3e113d31c5d15f26f6837344b5761b907a3e939e
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-21 09:49:38 -0600

    for loops for 4.01

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -2,10 +2,19 @@ for x in range(10):
     print("Hi")
 for x in range(5):
     print("Hello")
-for x in range(1):
-    print("There")
+print("There")
 for x in range(5):
     print("Hello")
     print("There")
-
-
+for i in range(10):
+    print(i)
+for i in range(1, 11):
+    print(i)
+for i in range(10):
+    print(i+1)
+for i in range(2, 12, 2):
+    print(i)
+for i in range(5):
+    print((i+1)*2)
+for i in range(10, 0, -1):
+    print(i)
