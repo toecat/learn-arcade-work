4f2332c365e15c65496992821689f43cf8b1c688
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-02-24 10:31:15 -0600

    end save

--- Lab 03 - Draw Using Functions/coin_flipping.py
+++ Lab 03 - Draw Using Functions/coin_flipping.py
@@ -4,10 +4,10 @@ for i in range(50):
     heads = 0
     tails = 0
     if x> 50:
-        heads=+1
+        heads+=1
     if x<50:
-        tails=+1
+        tails+=1
 if heads>tails:
     print("Heads won")
-if tails>heads:
-    print("Tails won")        
+else:
+    print("Tails won")      
---
+++ Lab 04 - Camel/lesson_2.py
@@ -0,0 +1,47 @@
+for x in range(10):
+    print("*", end=" ")
+for x in range(10):
+    print("*", end=" ")
+print()
+for i in range(5):
+    print("*", end=" ")
+print()
+for j in range(20):
+    print("*", end=" ")
+
+for x in range(10):
+    print()
+    for i in range(10):
+        print("*", end=" ")
+for x in range(10):
+    print()
+    for i in range(5):
+        print("*", end=" ")
+for x in range(5):
+    print()
+    for i in range(20):
+        print("*", end=" ")
+for row in range(10):
+    for column in range(10):
+        print(column, end=" ")
+    print()
+for row in range(10):
+    for column in range(10):
+        print(row, end=" ")
+    print()
+for row in range(10):
+    for coulmn in range(row+1):
+        print(coulmn, end=" ")
+    print()
+for row in range(10):
+    for j in range(row):
+        print(" ", end=" ")
+    for j in range(10 - row):
+        print(j, end=" ")
+    print()
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i * j < 10:
+            print(" ", end="")
+        print(i * j, end=" ")
+    print()
