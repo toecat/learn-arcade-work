e41ba2f8fdcec8f646967d339517aaadb7ad9fa8
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-03-01 09:59:01 -0600

    seperates them with a print() statement

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,20 +1,28 @@
 for x in range(10):
     print("Hi")
+print()
 for x in range(5):
     print("Hello")
 print("There")
+print()
 for x in range(5):
     print("Hello")
     print("There")  
+print()
 for i in range(10):
     print(i)
+print()
 for i in range(1, 11):
     print(i)
+print()
 for i in range(10):
     print(i+1)
+print()
 for i in range(2, 12, 2):
     print(i)
+print()
 for i in range(5):
     print((i+1)*2)
+print()
 for i in range(10, 0, -1):
     print(i)
