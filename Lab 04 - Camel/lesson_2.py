fd2923bf30b78f5729e138b49ba5fa15b3ceb86d
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-03-02 10:05:21 -0600

    finished

--- Lab 04 - Camel/lesson_2.py
+++ Lab 04 - Camel/lesson_2.py
@@ -45,3 +45,57 @@ for i in range(1, 10):
             print(" ", end="")
         print(i * j, end=" ")
     print()
+for i in range(10):
+    
+    for j in range(10 - i):
+        print (" ", end=" ")
+    
+    for j in range(1, i + 1):
+        print (j, end=" ")
+    
+    for j in range(i - 1, 0, -1):
+        print (j, end=" ")
+    
+    print()
+for i in range(10):
+    
+    for j in range(10 - i):
+        print (" ", end=" ")
+    
+    for j in range(1, i + 1):
+        print (j, end=" ")
+    
+    for j in range(i - 1, 0, -1):
+        print (j, end=" ")
+    
+    print()
+for row in range(10):
+    for j in range(row):
+        print(" ", end=" ")
+    for j in range(10 - row):
+        print(j, end=" ")
+    print()
+for i in range(10):
+    
+    for j in range(10 - i):
+        print (" ", end=" ")
+    
+    for j in range(1, i + 1):
+        print (j, end=" ")
+    
+    for j in range(i - 1, 0, -1):
+        print (j, end=" ")
+    
+    print()
+for i in range(10):
+   
+    for j in range(i + 2):
+        print(" ", end=" ")
+   
+    for j in range(1, 9 - i):
+        print(j, end=" ")
+    
+    for j in range(7 - i, 0, -1):
+        print(j, end=" ")
+    print()
+
