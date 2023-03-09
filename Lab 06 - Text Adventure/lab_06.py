3eeaf4cf4d00e3b1e3a18ba6b3f4bbd18daa9334
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-03-09 10:29:57 -0600

    adds second set of names

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,3 +1,28 @@
-names = ["Chloe", "Kate", "Sloane", "Macie", "Max"]
+names = ["Chloe", "Kate", "Sloane", "Macie", "Grace"]
+
 for item in names:
     print(item)
+
+x=0
+print("Hello,", names[x], "you have been invited to my party")
+x+=1
+print("Hello,", names[x], "you have been invited to my party")
+x+=1
+print("Hello,", names[x], "you have been invited to my party")
+x+=1
+print("Hello,", names[x], "you have been invited to my party")
+x+=1
+print("Hello,", names[x], "you have been invited to my party")
+
+my_list=["Alesha", "Cianna","Shelby"]
+for item in my_list:
+    print(item)
+x=0
+print("Hello,", my_list[x], "you have been invited to my party")
+x+=1
+print("Hello,", my_list[x], "you have been invited to my party")
+x+=1
+print("Hello,", my_list[x], "you have been invited to my party")
+
+
+
