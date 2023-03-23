11fd886f73801fd85bf88831ddefcfbb54086de9
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-03-22 20:11:54 -0500

    binary search

--- Lab 06 - Text Adventure/binary_search.py
+++ Lab 06 - Text Adventure/binary_search.py
@@ -1,4 +1,30 @@
 # -- Put your definition for binary_search right below:
+# --- Binary search
+def binary_search(my_list, key): 
+    
+    lower_bound = 0
+    upper_bound = len(my_list)-1
+    found = False
+
+    # Loop until we find the item, or our upper/lower bounds meet
+    while lower_bound <= upper_bound and not found:
+
+        # Find the middle position
+        middle_pos = (lower_bound + upper_bound) // 2
+
+        # Figure out if we:
+        # move up the lower bound, or
+        # move down the upper bound, or
+        # we found what we are looking for
+        if my_list[middle_pos] < key:
+            lower_bound = middle_pos + 1
+        elif my_list[middle_pos] > key:
+            upper_bound = middle_pos - 1
+        else:
+            found = True
+            return(middle_pos)
+    return(-1)
+
 
 # -- Now if the function works, all these tests should pass:
 my_list = [0, 3, 5, 12, 18, 50, 70, 78]
