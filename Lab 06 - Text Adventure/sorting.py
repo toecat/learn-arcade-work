3200066eed5fe113e7c556006869fb182771b4dd
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-03-27 09:56:13 -0500

    gets rid of import random

--- Lab 06 - Text Adventure/sorting.py
+++ Lab 06 - Text Adventure/sorting.py
@@ -1,15 +1,11 @@
-import random
-my_list = [97,74,8,98,47,62,12,11,0,60]
 def selection_sort(my_list):
 
     for cur_pos in range(len(my_list)):
-
         min_pos = cur_pos
 
         for scan_pos in range(cur_pos + 1, len(my_list)):
 
             if my_list[scan_pos] < my_list[min_pos]:
-
                 min_pos = scan_pos
         print_list(my_list)
         temp = my_list[min_pos]
@@ -22,6 +18,5 @@ def print_list(my_list):
     print()
 
 my_list = [97,74,8,98,47,62,12,11,0,60]
-print_list(my_list)
 selection_sort(my_list)
 print_list(my_list)
