14581091fc15aca0364f12a2f9899c704bdcb7cb
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-03-30 09:21:37 -0500

    save

--- Lab 06 - Text Adventure/recursion.py
+++
@@ -1,9 +0,0 @@
-my_list = [70, 32, 98, 88, 92, 36, 81, 83, 87, 66]
-temp = my_list[0]
-print(temp)
-print(my_list)
-my_list[1] = my_list[0]
-print(my_list)
-my_list[0] = temp
-print(temp)
-print(my_list)
