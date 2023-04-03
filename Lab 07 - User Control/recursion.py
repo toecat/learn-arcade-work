522d53ddae663289f9efbb0cbb7b2636ab1944a5
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-02 21:22:52 -0500

    save 1

--- Lab 07 - User Control/recursion.py
+++ Lab 07 - User Control/recursion.py
@@ -14,20 +14,33 @@ def main():
         line = line.strip()
         dictionary_list.append(line)
     #print (dictionary_list)
+    list_position = linear_search(my_file)
+
+    if list_position < len(word_list):
+        print("The name", my_file, "is at position", list_position)
+    else:
+        print("The name", my_file, "was not in the list.")
 
     my_file.close()
 
+
+
     file = open("Lab 07 - User Control/AliceInWonderLand200.txt")
     for aline in file:
+
+
         word_list = split_line(aline)
         for word in word_list:
-                # Start at the beginning of the list
-                #print(word)
-                current_list_position = 0
-                # Loop until you reach the end of the list, or the value at the
-                # current position is equal to the key
-                while current_list_position < len(word) and dictionary_list[current_list_position] != word:
-                # Advance to the next item in the list
+
+
+
+                def linear_search():
+                    current_list_position = 0
+
+                    while current_list_position < len(word) and dictionary_list[current_list_position] != word:
+                        current_list_position += 1
+
+                        return current_list_position
                     print (dictionary_list[current_list_position], " " , word)
                     current_list_position += 1
                     if current_list_position < len(word_list):
