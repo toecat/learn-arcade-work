c5c031bee14140e1e9d6850ae29d6cb51c411b9a
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-02 21:57:08 -0500

    goes through and looks at all the words

--- Lab 07 - User Control/recursion.py
+++ Lab 07 - User Control/recursion.py
@@ -3,51 +3,43 @@ import re
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
 
+def linear_search(w,dictionary_list):
+    key = w.upper()
 
+    current_list_position = 0
+
+    while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != key:
+
+        current_list_position += 1
+
+    if current_list_position < len(dictionary_list):
+        return
 
 def main():
  
     my_file = open("Lab 07 - User Control/dictionary.txt")
-
+    
     dictionary_list = []
     for line in my_file:
         line = line.strip()
         dictionary_list.append(line)
-    #print (dictionary_list)
-    list_position = linear_search(my_file)
-
-    if list_position < len(word_list):
-        print("The name", my_file, "is at position", list_position)
-    else:
-        print("The name", my_file, "was not in the list.")
-
-    my_file.close()
-
-
-
     file = open("Lab 07 - User Control/AliceInWonderLand200.txt")
+    
+    
     for aline in file:
 
 
         word_list = split_line(aline)
         for word in word_list:
+            linear_search(word, dictionary_list)
+            print(word)                        
+
+   
+    my_file.close()
 
 
 
-                def linear_search():
-                    current_list_position = 0
-
-                    while current_list_position < len(word) and dictionary_list[current_list_position] != word:
-                        current_list_position += 1
-
-                        return current_list_position
-                    print (dictionary_list[current_list_position], " " , word)
-                    current_list_position += 1
-                    if current_list_position < len(word_list):
-                        current_list_position += 1
-                    else:
-                        print("The name was not in the list, ", word)
-
+  
 
 
 
