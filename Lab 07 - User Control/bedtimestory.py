71a657a044bf4d64d848182c88fb99e3794c17e2
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-04 09:46:32 -0500

    save

--- Lab 07 - User Control/bedtimestory.py
+++ Lab 07 - User Control/bedtimestory.py
@@ -1,5 +1,3 @@
-
-
 def bedtimestory(animal_list,current,number): 
     print(animal_list [current],"went to bed after reading a story to the", animal_list[current+1])                  
     if current == number:
