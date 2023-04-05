1dc2ae1cd7647660282dada10e73c69a0ff9a788
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-05 18:45:16 -0500

    save

--- Lab 09 - Sprites and Walls/classes.py
+++ Lab 09 - Sprites and Walls/classes.py
@@ -2,21 +2,22 @@ import arcade
 import random
 
 class Rectangle():
-    def __init__(self):
-        self.x = random.randrange(0, 700)
-        self.y = random.randrange(0, 500)
-        self.height = random.randrange(20,70)
-        self.width = random.randrange(20,70)
-        self.color = [random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)]
-        self.change_x = random.randrange(-3,3)
-        self.change_y = random.randrange(-3,3)   
+    def __init__(self,x,y,width,height,change_x,change_y,color):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.color = color
+        self.change_x = change_x
+        self.change_y = change_y  
+
     
     def move(self): 
         self.x += self.change_x
         self.y += self.change_y
         
     def draw(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.height,self.width, self.color)
+        arcade.draw_rectangle_filled(self.x, self.y, self.width,self.height, self.color)
 
 
 class Ellipse(Rectangle):  
@@ -25,23 +26,45 @@ class Ellipse(Rectangle):
     def draw(self):
         arcade.draw_ellipse_filled(self.x, self.y, self.height, self.width, self.color)
 
-
-def main():
-    arcade.open_window(700, 700, "Window")
+class Shapes(arcade.Window):
+    def _init_(self):
+        super()._init_(700,700,"Window")
+        self.my_list=None
     
-    arcade.start_render()
+    def setup(self):
+        self.my_list=[]
+        for x in range(52):
+            x=random.randrange(0,700)
+            y=random.randrange(0,500)
+            height=random.randrange(20,70)
+            width=random.randrange(20,70)
+            change_x=random.randrange(-3,3)
+            change_y=random.randrange(-3,3)
+
+            todraw = random.randrange(1,3)
+            if todraw == 1:
+                my_object = Rectangle(x+random.randrange(25),y,width,height,change_x,change_y,[random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)])
+                self.my_list.append(my_object)
+            if todraw == 2:
+                my_ellipse = Ellipse(x,y+random.randrange(70),width,height,change_x,change_y,[random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)])
+                self.my_list.append(my_ellipse)
+
     
-    my_list = []
-    for x in range(30):
-        my_object = Rectangle()
-        my_ellipse = Ellipse()
-        my_object.draw()
-        my_ellipse.draw()
-    my_list.append(my_object)
-    my_list.append(my_ellipse)
+    def update(self,dt):
+        for shapes in self.my_list:
+            shapes.move()
     
-    arcade.run()
-    arcade.finish_render()
+    
+    def on_draw(self):
+        arcade.start_render()
+        for shape in self.my_list:
+            shape.draw()
+    
+   
+window=Shapes()
+window.setup()
+arcade.run()
 
 
-main()        
+
+      
