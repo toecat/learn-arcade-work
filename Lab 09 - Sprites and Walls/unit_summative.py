e2ea5cc0f3ec786df0e7668603bd23cd693d9ab6
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-06 10:18:00 -0500

    save

--- Lab 09 - Sprites and Walls/unit_summative.py
+++ Lab 09 - Sprites and Walls/unit_summative.py
@@ -1,15 +1,17 @@
 class Room():
-    def _init_(self, description, north, east, south, west):
-        self.description =  description
+    def _init_(self,description, north, east, south, west):
+        self.description = description
         self.north = north
         self.east = east
         self.south = south
         self.south = west
-    
 
-def main(descripton, north, east, south, west):
+
+def main():
+    room = Room("bathroom", 9, 1, None, None)
     room_list = []
-    room = Room("This is the bedroom", north, east, None, None
-    room.append(room_list)
+    room_list.append(room)
+    
+    print(room_list)
 
 main()
