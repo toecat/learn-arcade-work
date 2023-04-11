9508c055180e70093bbab43470ffb2addcaaa3ba
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-11 10:38:52 -0500

    save

--- Lab 09 - Sprites and Walls/unit_summative.py
+++ Lab 09 - Sprites and Walls/unit_summative.py
@@ -8,17 +8,17 @@ class Room():
 
 
 def main():
-    bathroom = Room("bathroom 1", 9, 1, None, None) 
-    bedroom = Room("bedroom 1", 8, 2, None, 0)
-    southhallway = Room("south hallway", 7, 3, None, 1)
-    kitchen = Room("kitchen", 4, None, None,2)  
-    livingroom = Room("living room", 5, None,3,7)
-    diningroom = Room("dining room",6, None, 4, 10)
-    bedroom2 = Room("bedroom 2", None, None,5, None)
-    northhallway = Room("north hallway", 10, 4, 2, 8)
-    bedroom3 = Room("bedroom 3", None,7,1,9)
-    bathroom3 = Room("bathroom 3", None, 8, 0, None)
-    balcony = Room("balcony", None, None, 7, None)
+    bathroom = Room("You are in bathroom 1. There is a door to the north and east.", 9, 1, None, None) 
+    bedroom = Room("You are in bedroom 1. There is a door to the north, east, and west.", 8, 2, None, 0)
+    southhallway = Room("You are in the south hallway. There is a door to the north, east, and west.", 7, 3, None, 1)
+    kitchen = Room("You are in the kitchen. There is a door to the north and west.", 4, None, None,2)  
+    livingroom = Room("You are in the living room. There is a door to the north, south, and west.", 5, None,3,7)
+    diningroom = Room("You are in the dining room. There is a door to the north, south, and west.",6, None, 4, 10)
+    bedroom2 = Room("You are in bedroom 2. There is a door to the south.", None, None,5, None)
+    northhallway = Room("You are in the north hallway. There is a door to the north, east, south, and west.", 10, 4, 2, 8)
+    bedroom3 = Room("You are in bedroom 3. There is a door to the east, south and west.", None,7,1,9)
+    bathroom3 = Room("You are in bathroom 3. There is a door to the east and south.", None, 8, 0, None)
+    balcony = Room("You are on the balcony. There is a door to the south.", None, None, 7, None)
     room_list = []
     room_list.append(bathroom)
     room_list.append(bedroom)
@@ -33,14 +33,30 @@ def main():
     room_list.append(balcony)
     current_room = 0
     done=False
-    user_input = input("Where would you like to go?")
-    if user_input == "north"():
-        print("hello")
     while done != True:
         print()
         print(room_list[current_room].description)
+
+        user_input = input("Where would you like to go?")
+
         done = True
-    #next_room = room_list(current_room).north
+
+    if user_input.lower() == "north":
+        next_room = room_list[current_room].north
+        
+        print(next_room)
+        if next_room == None:
+            print("no")
+        next_room = current_room
+        print(current_room)
+    elif user_input.lower()=="south":
+        print("south")
+    elif user_input.lower()=="east":
+        print("east")
+    elif user_input.lower()=="west":
+        print("west")
+        
+    
 
 
 main()
