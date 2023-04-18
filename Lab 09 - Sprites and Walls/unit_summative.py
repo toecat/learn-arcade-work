b5de57e44a33da4652d683e8f6308e8ce98dcf42
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-04-18 10:26:11 -0500

    save

--- Lab 09 - Sprites and Walls/unit_summative.py
+++ Lab 09 - Sprites and Walls/unit_summative.py
@@ -1,4 +1,5 @@
-print("Find the key and unlock the door but make sure that you are ready to fight the dragon. \nYou will need camoflauge clothes, a sword, and food to refuel. ")
+
+print("Find the key and unlock the door but make sure you are ready to fight the dragon. \nYou might want camoflauge clothes, a sword, and food to help you succeed in your mission.\nGo searching around different rooms to try and find these items before you fight the dragon.\nIf you would like to see what you have type invenotry. \nIf you would like to pick something up, type grab, then the item name such as clothes.\nTo type the direction that you would like to go type either north, east, south, or west.\nIf you would like to quit type quit.")
 
 class Room():
     def __init__(self,description, north, east, south, west, locked):
@@ -24,9 +25,9 @@ class Item():
 def main():
     room_list = []
 
-    bathroom = Room("You are in bathroom 1. \nThere is a door to the north and east.", 9, 1, None, None, False) 
+    bathroom = Room("You are in the first bathroom. \nThere is a door to the north and east.", 9, 1, None, None, False) 
     room_list.append(bathroom)
-    bedroom = Room("You are in bedroom 1. \nThere is a door to the north, east, and west.", 8, 2, None, 0, False)
+    bedroom = Room("You are in the first bedroom. \nThere is a door to the north, east, and west.", 8, 2, None, 0, False)
     room_list.append(bedroom)
     southhallway = Room("You are in the south hallway. \nThere is a door to the north, east, and west.", 7, 3, None, 1, False)
     room_list.append(southhallway)
@@ -36,21 +37,21 @@ def main():
     room_list.append(livingroom)
     diningroom = Room("You are in the dining room. \nThere is a door to the north, south, and west.",6, None, 4, 10, False)
     room_list.append(diningroom)
-    bedroom2 = Room("You are in bedroom 2. \nThere is a door to the south.", None, None,5, None, False)
+    bedroom2 = Room("You are in the second bedroom. \nThere is a door to the south.", None, None,5, None, False)
     room_list.append(bedroom2)
     northhallway = Room("You are in the north hallway. \nThere is a door to the north, east, south, and west.", 10, 4, 2, 8, False)
     room_list.append(northhallway)
-    bedroom3 = Room("You are in bedroom 3. \nThere is a door to the east, south and west.", None,7,1,9, False)
+    bedroom3 = Room("You are in the third bedroom. \nThere is a door to the east, south and west.", None,7,1,9, False)
     room_list.append(bedroom3)
-    bathroom3 = Room("You are in bathroom 3. \nThere is a door to the east and south.", None, 8, 0, None, False)
+    bathroom3 = Room("You are in the third bathroom. \nThere is a door to the east and south.", None, 8, 0, None, False)
     room_list.append(bathroom3)
-    balcony = Room("You are on the balcony. \nThere is a door to the south.", None, None, 7, None, True)
+    balcony = Room("You are on the balcony where the dragon is. \nThere is a door to the south.", None, None, 7, None, True)
     room_list.append(balcony)
     current_room = 0
     key =  Item("key", "There is a key in this room", 2, "used the key unlock the door")
     sword = Item("sword", "There is a sword in this room", 5, "used the sword to kill the dragon")
-    food = Item("rusty", "There is food in here", 3, "I ate the food to refuel.")
-    clothes = Item("clothes", "There is camoflauge clothes in this room", 8, "used the clothes to change my camoflauge outfit")
+    food = Item("food", "There is food in here", 3, "ate the food to refuel.")
+    clothes = Item("clothes", "There is camoflauge clothes in this room", 8, "used the clothes to change into my camoflauge outfit.")
 
 
     items=[]
@@ -75,20 +76,31 @@ def main():
         while x<4:  
             if items[x].room_number == current_room:
                 print(items[x].long)
-                break   
+                break
             x+=1
         if current_room == 10:
-            user_inputs = input("Are you ready to fight the dragon?")
-            if user_inputs == "yes":
-                for item in items:
-                    if item.room_number == -1:
-                        print("i have tiems")
-
-
+            print("You inventory includes:", items_held.short_name)
+            ask = input("What would you like to use from your inventory to fight the dragon:")
+            if ask == "clothes":
+                print("You were able to camoflauge yourself and get behind the dragon and get the treasure.")
+                break
+                    
+            if ask == "key":
+                print("The key killed the dragon and you were able to collect the treasure.")
+                break
+                    
+            if ask == "sword":
+                print("You were able to kill the dragon with your sword and were able to get to the treasure.")
+                break
+            if ask == "food":
+                print("You fed the dragon and made it stronger which killed you. You did not collect the treasure")
+                break
+                    
 
 
         user_input = input("What would you like to do? ")
-        print()
+        print("--------------------")
+
 
         
 
@@ -178,7 +190,7 @@ def main():
 
             room_lock = room_list[next_room].locked
             if room_lock == True:
-                print("find the key and come back")
+                print("Find or use the key to unlock this room.")
             if room_lock == False:
                 print(room_list[next_room].description)
                 current_room = next_room
