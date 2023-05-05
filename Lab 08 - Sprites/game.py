25f00555568eb8ad8cf1d5510ec71a6ccad15837
toecat <tbutterfass.2026@students.rfsd.k12.wi.us>
2023-05-05 08:43:15 -0500

    Saves

--- Lab 08 - Sprites/game.py
+++ Lab 08 - Sprites/game.py
@@ -1,34 +1,45 @@
-"""
-Platformer Game
-"""
 import arcade
 
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer Game"
+SCREEN_TITLE = "Platformer"
+
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
-PLAYER_MOVEMENT_SPEED = 5
-PLAYER_JUMP_SPEED = 20
+COIN_SCALING = 0.5
+
+PLAYER_MOVEMENT_SPEED = 10
 GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
 
 class MyGame(arcade.Window):
- 
+
 
     def __init__(self):
 
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        self.scene = None 
+        self.tile_map = None
 
-        self.wall_list = None
-        self.player_list = None
+        self.scene = None
 
         self.player_sprite = None
+
         self.physics_engine = None
 
+        self.camera = None
 
+        self.gui_camera = None
+
+        self.score = 0
+
+        
+        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
 
@@ -38,88 +49,93 @@ class MyGame(arcade.Window):
         map_name = ":resources:tiled_maps/map.json"
 
         layer_options = {"Platforms": {"use_spatial_hash": True,},}
-        
+
         self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
 
-
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
-        self.scene.add_sprite_list("Player")
-        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
-        self.score = 0
 
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.score = 0
 
         image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
+        self.player_sprite.center_x = 128
         self.player_sprite.center_y = 128
         self.scene.add_sprite("Player", self.player_sprite)
 
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
 
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"])
-
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.scene.add_sprite("Walls", wall)
-
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
-
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
-            wall.position = coordinate
-            self.scene.add_sprite("Walls", wall)
-            self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.scene.get_sprite_list("Walls"))
-
-
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"]
+        )
 
     def on_draw(self):
 
         self.clear()
 
-        self.wall_list.draw()
-        self.player_list.draw()
+        self.camera.use()
+
         self.scene.draw()
 
+        self.gui_camera.use()
+
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(
+            score_text,
+            10,
+            10,
+            arcade.csscolor.WHITE,
+            18,
+        )
+
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed."""
 
         if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                arcade.play_sound(self.jump_sound)
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        """Called when the user releases a key."""
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
 
-    def on_update(self, delta_time):
-        """Movement and game logic"""
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
 
-        # Move the player with the physics engine
+        self.camera.move_to(player_centered)
+
+    def on_update(self, delta_time):
+
         self.physics_engine.update()
 
+        coin_hit_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.scene["Coins"]
+        )
 
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
+            arcade.play_sound(self.collect_coin_sound)
+            self.score += 1
+
+        self.center_camera_to_player()
 
 
 def main():
-    """Main function"""
     window = MyGame()
     window.setup()
     arcade.run()
